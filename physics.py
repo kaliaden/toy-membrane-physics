21726c602f6ce761c6c929dffb6929ff430949d4
kaliaden <techhans@gmail.com>
2016-06-22 12:43:03 +0530

    add spring force and euclidean distance

--- physics.py
+++ physics.py
@@ -1,1 +1,8 @@
 ''' Implements basic physics for bead and spring models '''
+
+import Bead
+import scipy
+
+def spring_force(atom1, atom2, k):
+  r = scipy.spatial.distance.euclidean(atom1.x, atom2.x)
+  return -k*r
