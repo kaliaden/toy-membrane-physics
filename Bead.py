c559ab65622d6a9cc624c6339914d7e9696df02b
kaliaden <techhans@gmail.com>
2017-11-30 16:24:31 +0530

    Includes new description of module. Pending

--- Bead.py
+++ Bead.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+'''Rewrite bead to represent each molecule as an undirected graph with
+node weight = mass and edge weight equals spring constant. Some of this
+has been partially implemented in physics. quite badly...'''
+
 class Bead:
     def __init__(self, x, v, a, arg):
         self.x = np.asarray(x) #Position
@@ -7,13 +11,14 @@ class Bead:
         self.a = np.asarray(a) #Acceleration
         self.arg = arg
         self.bonds = []
-    
+
     def bondTo(self, bead):
         self.bonds.append(bead)
         bead.bonds.append(self)
-    
+
     def isBonded(self, bead):
         return bead in self.bonds
 
     def draw(self):
         pass
+
